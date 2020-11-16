d153d3de2308ac6c05acfb70ece4dab2054f548f
Mohamed Hussein <mohamed.hussein151515@gmail.com>
2020-11-16 21:09:33 +0200

    Implemented_encryption_and_decryption_feature

--- src/chat/Client.java
+++ src/chat/Client.java
@@ -13,10 +13,14 @@ public class Client extends javax.swing.JFrame {
     ObjectInputStream in;
     private String name;
     
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
+    private int encryptionKey = 0;
+    private int decryptionKey = 0;
+
+    public int getEncryptionKey() { return encryptionKey; }
+    public void setEncryptionKey(int encryptionKey) { this.encryptionKey = encryptionKey; }
+    public int getDecryptionKey() { return decryptionKey; }
+    public void setDecryptionKey(int decryptionKey) { this.decryptionKey = decryptionKey; }
+    public void setName(String name) { this.name = name; }
 
     public Client() {
         initComponents();
@@ -25,9 +29,7 @@ public class Client extends javax.swing.JFrame {
             s = new Socket("localhost", 3333);
             out = new ObjectOutputStream(s.getOutputStream());
             in = new ObjectInputStream(s.getInputStream());
-        } catch(Exception ex) {
-            System.out.println(ex);
-        }
+        } catch(Exception ex) { System.out.println(ex); }
          
         jButton1.setEnabled(false);
         jTextField1.setEnabled(false);
@@ -41,12 +43,11 @@ public class Client extends javax.swing.JFrame {
         
         try {
             String time = getTime();
-            Message msg = new Message("MESSAGE" ,name, text, time);
-            out.writeObject(msg);
+            Message msg = new Message("MESSAGE", name, text, time);
             jTextArea1.append(createMessageBlock(msg));
-        } catch(Exception ex) {
-            System.out.println(ex);
-        }
+            msg.setText(encryptText(text));
+            out.writeObject(msg);
+        } catch(Exception ex) { System.out.println(ex); }
         
         jTextField1.setText("");
     }
@@ -59,7 +60,7 @@ public class Client extends javax.swing.JFrame {
                 
                 switch(msg.getType()) {
                     case "MESSAGE":
-                        jTextArea1.append(createMessageBlock(msg));
+                        jTextArea1.append(createReceivedMessageBlock(msg));
                         break;
                     case "CONFIRM_USER":
                         confirmUserName(msg.getAuther());
@@ -72,9 +73,7 @@ public class Client extends javax.swing.JFrame {
                         break;
                 }
             }
-        } catch(Exception ex) {
-            System.out.println(ex);
-        }
+        } catch(Exception ex) { System.out.println(ex); }
     }
     
     public void setUserName() {
@@ -85,9 +84,7 @@ public class Client extends javax.swing.JFrame {
         
         try {
             out.writeObject(new Message("SET_USER_NAME", name));
-        } catch(Exception ex) {
-            System.out.println(ex);
-        }
+        } catch(Exception ex) { System.out.println(ex); }
     }
     
     public void confirmUserName(String name) {
@@ -110,7 +107,29 @@ public class Client extends javax.swing.JFrame {
         return msg.getAuther() + ": " + msg.getText() + "\t" + msg.getTime() + "\n";
     }
     
+    public String createReceivedMessageBlock(Message msg) {
+        return msg.getAuther() + ": " + decryptText(msg.getText()) + "\t" + msg.getTime() + "\n";
+    }
+    
     public void clearChat() { jTextArea1.setText(""); }
+    
+    public String encryptText(String text) {
+        String encrypted = "";
+        
+        for (int x = 0; x < text.length(); x++)
+            encrypted += (char) (text.charAt(x) + getEncryptionKey());
+        
+        return encrypted;
+    }
+    
+    public String decryptText(String text) {
+        String decrypted = "";
+        
+        for (int x = 0; x < text.length(); x++)
+            decrypted += (char) (text.charAt(x) - getDecryptionKey());
+        
+        return decrypted;
+    }
 
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -124,6 +143,12 @@ public class Client extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
+        jTextField3 = new javax.swing.JTextField();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+        jTextField4 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -183,6 +208,41 @@ public class Client extends javax.swing.JFrame {
             }
         });
 
+        jTextField3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField3ActionPerformed(evt);
+            }
+        });
+        jTextField3.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jTextField3KeyPressed(evt);
+            }
+        });
+
+        jButton5.setText("Encryption");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+
+        jButton6.setText("Decryption");
+        jButton6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton6ActionPerformed(evt);
+            }
+        });
+
+        jTextField4.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jTextField4KeyPressed(evt);
+            }
+        });
+
+        jLabel2.setText("0");
+
+        jLabel3.setText("0");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -196,16 +256,25 @@ public class Client extends javax.swing.JFrame {
                             .addComponent(jTextField1))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 121, Short.MAX_VALUE)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 124, Short.MAX_VALUE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(102, 102, 102)))
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jButton4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addComponent(jButton4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton5, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel2))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel3)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -219,14 +288,25 @@ public class Client extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jScrollPane1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED))
+                        .addComponent(jScrollPane1))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(54, 54, 54)
+                        .addGap(36, 36, 36)
                         .addComponent(jButton3)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jButton4)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 141, Short.MAX_VALUE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 39, Short.MAX_VALUE)
+                        .addComponent(jButton5)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton6)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1))
@@ -254,9 +334,8 @@ public class Client extends javax.swing.JFrame {
     }//GEN-LAST:event_jTextField2ActionPerformed
 
     private void jTextField2KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField2KeyPressed
-        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER)
             setUserName();
-        }
     }//GEN-LAST:event_jTextField2KeyPressed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -271,10 +350,44 @@ public class Client extends javax.swing.JFrame {
         try {
             out.writeObject(new Message("CLEAR_CHAT"));
             clearChat();
-        } catch(Exception ex) {
-            System.out.println(ex);
-        }
+        } catch(Exception ex) { System.out.println(ex); }
     }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void setupEncryption() {
+        String key = jTextField3.getText();
+        setEncryptionKey(Integer.parseInt(key));
+        jLabel2.setText(key);
+        jTextField3.setText("");
+    }
+    
+    private void setupDecryption() {
+        String key = jTextField4.getText();
+        setDecryptionKey(Integer.parseInt(key));
+        jLabel3.setText(key);
+        jTextField4.setText("");
+    }
+    
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        setupEncryption();
+    }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
+        
+    }//GEN-LAST:event_jTextField3ActionPerformed
+
+    private void jTextField3KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField3KeyPressed
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER)
+            setupEncryption();
+    }//GEN-LAST:event_jTextField3KeyPressed
+
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
+        setupDecryption();
+    }//GEN-LAST:event_jButton6ActionPerformed
+
+    private void jTextField4KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField4KeyPressed
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER)
+            setupDecryption();
+    }//GEN-LAST:event_jTextField4KeyPressed
 
     public static void main(String args[]) {
         
@@ -317,10 +430,16 @@ public class Client extends javax.swing.JFrame {
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
     // End of variables declaration//GEN-END:variables
 }
